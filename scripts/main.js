6fadeaf69c2cbc93a9c18487a15b79bc45ea0637
morfeii <morfeii88@gmail.com>
2020-02-10 19:21:24 +0200

    Updates

--- scripts/main.js
+++ scripts/main.js
@@ -8,17 +8,18 @@ const nextBtn = document.querySelector('.carousel__btn_next');
 
 let counter = 0;
 const size = 330;
+const transitionValue = 'transform 0.4s ease-in-out';
 
-carousel.style.transform = 'translateX(' + (-size * counter) + 'px)';
+carousel.style.transform = `translateX(${-size * counter}px)`;
 
 nextBtn.addEventListener('click', () => {
   if (counter >= cards.length - 1) {
     return;
   }
 
-  carousel.style.transition = 'transform 0.4s ease-in-out';
+  carousel.style.transition = transitionValue;
   counter += 1;
-  carousel.style.transform = 'translateX(' + (-size * counter) + 'px)';
+  carousel.style.transform = `translateX(${-size * counter}px)`;
 
   dots[counter].classList.add('carousel__dot_active');
   dots[counter - 1].classList.remove('carousel__dot_active');
@@ -29,9 +30,9 @@ prevBtn.addEventListener('click', () => {
     return;
   }
 
-  carousel.style.transition = 'transform 0.4s ease-in-out';
+  carousel.style.transition = transitionValue;
   counter -= 1;
-  carousel.style.transform = 'translateX(' + (-size * counter) + 'px)';
+  carousel.style.transform = `translateX(${-size * counter}px)`;
 
   dots[counter].classList.add('carousel__dot_active');
   dots[counter + 1].classList.remove('carousel__dot_active');
